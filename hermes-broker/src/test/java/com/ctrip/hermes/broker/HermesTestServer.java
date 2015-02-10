48084eaddd56b3c49e7f23816ce9050b7b7d3f6e
marsqing <q_gu@ctrip.com>
2015-02-10 16:55:47 +0800

    set to dev mode

--- hermes-broker/src/test/java/com/ctrip/hermes/broker/HermesTestServer.java
+++ hermes-broker/src/test/java/com/ctrip/hermes/broker/HermesTestServer.java
@@ -1,11 +1,17 @@
 package com.ctrip.hermes.broker;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.unidal.lookup.ComponentTestCase;
 
 import com.ctrip.hermes.remoting.netty.NettyServer;
 
 public class HermesTestServer extends ComponentTestCase {
+
+	@BeforeClass
+	public static void beforeClass() {
+		System.setProperty("devMode", "true");
+	}
 
 	@Test
 	public void test() throws Exception {
