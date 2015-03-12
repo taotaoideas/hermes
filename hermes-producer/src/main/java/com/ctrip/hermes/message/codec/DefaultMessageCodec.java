2a50953df0d1af2ebf9f3353d6dc2c94e9ba3bfb
ske <ske@Ctrip.com>
2015-03-12 10:37:58 +0800

    update Broker, Container, Example pom, fix CI problem: Example module need to depend on war projects(Broker and Container)

--- hermes-producer/src/main/java/com/ctrip/hermes/message/codec/DefaultMessageCodec.java
+++ hermes-producer/src/main/java/com/ctrip/hermes/message/codec/DefaultMessageCodec.java
@@ -7,6 +7,36 @@ import org.unidal.lookup.annotation.Inject;
 
 import com.ctrip.hermes.message.Message;
 
+/**
+ *
+  Introduce:
+
+	              Header                         Body(Bytes)
+	 +----+--------------------------------+ +----------------------+
+	 |0x00| Topic(String) Group(String)    | |                      |
+	 |    | Key(String) Partition (String) | |                      |
+	 |    | Properties(Map)                | |                      |
+	 |    | BodyLength(Int) ...            | |                      |
+	 +----+--------------------------------+ +----------------------+
+
+	 Element      Prefix
+	 +--------------------------------------------------------------+
+	 Fixed Width:                          Following Octets
+	 boolean      0x10(false) 0x11(true)   0 OCTET
+	 char         0x20                     2 OCTET
+	 int          0x30                     4 OCTET
+	 long         0x40                     8 OCTET
+	 +--------------------------------------------------------------+
+	 Variable Width:                       Following Data Size
+	 String       0xA2                     2OCTET  2^16 = 64K
+	 +----------------------------------------------+---------------+
+	 Array:       [size][count][Element][...]
+                  0xC1                     followed 1 octet of data count
+                  0xD4                     followed 4 octet of data count
+	 Map:         [size][count][[key Element][value Element]][...]
+	              0xE1                     followed 1 octet of data count
+                  0xF4                     followed 4 octet of data count
+ */
 public class DefaultMessageCodec implements MessageCodec {
 
 	@Inject
