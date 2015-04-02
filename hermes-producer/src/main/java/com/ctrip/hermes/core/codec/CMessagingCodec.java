b24b226e65cdcdc050fecab61ec14b9d22e83dd7
marsqing <q_gu@ctrip.com>
2015-04-02 15:40:01 +0800

    add CMessaging codec

---
+++ hermes-producer/src/main/java/com/ctrip/hermes/core/codec/CMessagingCodec.java
@@ -0,0 +1,34 @@
+package com.ctrip.hermes.core.codec;
+
+import java.util.Map;
+
+import com.google.common.base.Charsets;
+
+public class CMessagingCodec implements Codec {
+
+	@Override
+	public byte[] encode(String topic, Object obj) {
+		if (obj instanceof String) {
+			return ((String) obj).getBytes(Charsets.UTF_8);
+		} else {
+			throw new IllegalArgumentException("CMessaging producer messages should be String type, illegal message type "
+			      + obj.getClass());
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public <T> T decode(byte[] raw, Class<T> clazz) {
+		if (clazz == byte[].class) {
+			return (T) raw;
+		} else {
+			throw new IllegalArgumentException("CMessaging consumer messages should be byte[] type, illegal message type "
+			      + clazz);
+		}
+	}
+
+	@Override
+	public void configure(Map<String, ?> configs) {
+	}
+
+}
